991ec456c94fef22aaf63081236deb7c9c7417b3
Chris Kokolios <kokolios1986@yahoo.gr>
2016-08-25 00:08:45 +0300

    minor

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,9 @@ export default class App extends Component {
       <div className="container">
             
       		{this.props.children}
-      	
+        <p>Created By Christos Kokolios </p>
+        <p><a href="https://github.com/KokoChris/fccrecipe">Source Code</a></p>
+        <p><a href="https://github.com/KokoChris/recipeapi">Api Code</a></p>
       </div>
 
     );
