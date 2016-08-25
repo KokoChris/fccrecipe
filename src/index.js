8ffc1c9f2133bc1c75af574ca447129b509bb201
kokoChris <kokolios1986@yahoo.gr>
2016-08-25 16:59:19 +0300

    using sass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('bootstrap/dist/css/bootstrap.css');
+require('../style/style.scss');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,28 @@ module.exports = {
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
-    }]
+    },
+    {
+      test:/\.css$/,
+      loader:"style-loader!css-loader"
+    },
+    { 
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=100000' 
+    },
+    { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+      loader: "file-loader" 
+    },{
+      test:/\.scss$/,
+      exclude:/node_modules/,
+      loader:"style-loader!css-loader!sass-loader"
+
+    }
+
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx','.css']
   },
   devServer: {
     historyApiFallback: true,
